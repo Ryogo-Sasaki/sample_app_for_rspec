d96f11d8189e90e37932229016372594d56d269c
Ryogo-Sasaki <burusenpai@gmail.com>
2020-03-03 21:31:15 +0900

    [Add]model spec(task)を生成

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '全てのattributeがvalidationを通るか' do
+   task = Task.new(title: 'aaa', status: :doing)
+   expect(task).to be_valid
+  end
+
+    it 'タイトルがなかったらinvalid'do
+      task = Task.new(title: '', status: :doing )
+      expect(task.errors[:title]).to be_valid
+    end
+
+   it 'ステータスがなかったらinvalid' do
+     task = Task.new(title: 'aaa', status: '')
+    expect(task.errors[:status]).to be_valid
+   end
+
+  it 'タイトルが重複していたらinvalid' do
+    task = Task.new(title: 'bbb', status: :doing)
+    task.title = "bbb"
+    expect(task.errors).to be_valid
+  end
+
+  fit 'タイトルが重複していたらinvalid' do
+    task = Task.new(title: 'bbb', status: :doing)
+    task.title =  "ccc"
+    expect(task.errors).to be_valid
+  end
 end
