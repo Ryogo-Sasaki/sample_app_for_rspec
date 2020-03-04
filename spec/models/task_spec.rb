35dd104b7e4782204e5ec6ab7bf6ee4d3984896c
Ryogo-Sasaki <burusenpai@gmail.com>
2020-03-04 10:56:17 +0900

    [Add]わかりやすさを担保するため、it文の中の英語表記を追加する

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,30 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+  # ➀it 'is valid with all attributes' do end
   it '全てのattributeがvalidationを通るか' do
    task = Task.new(title: 'aaa', status: :doing)
    expect(task).to be_valid
   end
 
+  # ➁it 'is invalid without title' do end
     it 'タイトルがなかったらinvalid'do
       task = Task.new(title: '', status: :doing )
       expect(task.errors[:title]).to be_valid
     end
 
+  # ➂it 'is invalid without status' do end
    it 'ステータスがなかったらinvalid' do
      task = Task.new(title: 'aaa', status: '')
     expect(task.errors[:status]).to be_valid
    end
 
+  # ➃it 'is invalid with a duplicate title' do end
   it 'タイトルが重複していたらinvalid' do
     task = Task.new(title: 'bbb', status: :doing)
     task.title = "bbb"
-    expect(task.errors).to be_valid
+    expect(task.errors[:title]).to be_valid
   end
 
-  fit 'タイトルが重複していたらinvalid' do
+  # ➄it 'is valid with another title' do end
+  it 'タイトルが重複していたらinvalid' do
     task = Task.new(title: 'bbb', status: :doing)
     task.title =  "ccc"
-    expect(task.errors).to be_valid
+    expect(task.errors[:title]).to be_valid
   end
 end
