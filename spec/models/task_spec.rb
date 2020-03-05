1444d7a3e02140d8ceacfe7716c240c2b7e2c460
ryogo.sasaki <burusenpai@gmail.com>
2020-03-05 13:12:42 +0900

    [Fix]インデントを修正し、rspec(task)の➃、➄を修正する

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,33 +4,47 @@ RSpec.describe Task, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   # ➀it 'is valid with all attributes' do end
   it '全てのattributeがvalidationを通るか' do
-   task = Task.new(title: 'aaa', status: :doing)
-   expect(task).to be_valid
+    task = Task.new(title: 'aaa', status: :doing)
+    expect(task).to be_valid
   end
 
   # ➁it 'is invalid without title' do end
-    it 'タイトルがなかったらinvalid'do
-      task = Task.new(title: '', status: :doing )
-      expect(task.errors[:title]).to be_valid
-    end
+  it 'タイトルがなかったらinvalid' do
+    task = Task.new(title: '', status: :doing)
+    expect(task.errors[:title]).to be_valid
+  end
 
   # ➂it 'is invalid without status' do end
-   it 'ステータスがなかったらinvalid' do
-     task = Task.new(title: 'aaa', status: '')
+  it 'ステータスがなかったらinvalid' do
+    task = Task.new(title: 'aaa', status: '')
     expect(task.errors[:status]).to be_valid
-   end
+  end
 
   # ➃it 'is invalid with a duplicate title' do end
   it 'タイトルが重複していたらinvalid' do
-    task = Task.new(title: 'bbb', status: :doing)
-    task.title = "bbb"
+    Task.create(
+      tittle: 'aaa',
+      status: :doing
+    )
+    @task = Task.new(
+      tittle: 'aaa',
+      status: :doing
+    )
+    @task.valid?
     expect(task.errors[:title]).to be_valid
   end
 
   # ➄it 'is valid with another title' do end
   it 'タイトルが重複していたらinvalid' do
-    task = Task.new(title: 'bbb', status: :doing)
-    task.title =  "ccc"
+    Task.create(
+      tittle: 'aaa',
+      status: :doing
+    )
+    @task = Task.new(
+      tittle: 'bbb',
+      status: :doing
+    )
+    @task.valid?
     expect(task.errors[:title]).to be_valid
   end
 end
