c6b0755c316bc599b1c36e0304a2f619fb8d89ef
ryogo.sasaki <burusenpai@gmail.com>
2020-03-09 16:28:55 +0900

    [Add]system spec(users)を追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,67 +1,3 @@
-#   describe 'ログイン前' do
-#
-#     context 'フォームの入力値が正常' do
-#       let(:user) { User.new(email: 'aaa@gmail.com', password: 'aaa', password_confirmation: 'aaa', created_at: '', updated_at: '') }
-#       fit 'ユーザーの新規作成ができる' do
-#         expect(user).to have_selector '.alert-success', text: 'ユーザーの登録に成功しました'
-#       end
-#     end
-#
-#     context 'メールアドレスが未入力' do
-#       let(:user) { User.create(email: '', password: 'aaa', password_confirmation: 'aaa', created_at: '', updated_at: '') }
-#       fit 'ユーザーの新規作成が失敗する' do
-#         expect(user).to have_selector '.alert-danger', text: 'ユーザーの新規登録に失敗しました'
-#       end
-#     end
-#
-#
-#     context '登録済メールアドレスを使用' do
-#       let(:user) { User.create(email: 'aaa@gmail.com', password: 'aaa', password_confirmation: 'aaa', created_at: nil, updated_at: nil) }
-#       it 'ユーザーの新規作成が失敗する'
-#       user = User.new(email: 'aaa@gmail.com', password: 'aaa', password_confirmation: 'aaa', created_at: nil, updated_at: nil))
-#       expect(user).to have_selector '.alert_danger', text: 'ユーザー登録に失敗しました'
-#     end
-#
-#     describe 'マイページ' do
-#       context 'ログインしていない状態' do
-#         it 'マイページへのアクセスが失敗する'
-#       end
-#     end
-#   end
-#
-#
-#   describe 'ログイン後' do
-#     before do
-#       user = User.create!(email: 'aaa@gmail.com', password: 'aaa', password_confirmation: 'aaa', created_at: '', updated_at: '')
-#       visit login_path
-#     end
-#     describe 'ユーザー編集' do
-#       context 'フォームの入力値が正常' do
-#         before do
-#           visit edit_user_path
-#         end
-#       end
-#     end
-#     context 'メールアドレスが未入力時に' do
-#       it 'ユーザーの編集が失敗する'
-#     end
-#
-#     context '登録済メールアドレスを使用' do
-#       let(:email) { user.new(email: nil) }
-#
-#       it 'ユーザーの編集が失敗'
-#     end
-#     context '他ユーザーの編集ページにアクセス' do
-#       it 'アクセスが失敗する'
-#     end
-#
-#     describe 'マイページ' do
-#       context 'タスクを作成' do
-#         it '新規作成したタスクが表示される'
-#       end
-#     end
-#   end
-# end
 require 'rails_helper'
 
 RSpec.describe User, type: :system do
@@ -110,20 +46,20 @@ RSpec.describe User, type: :system do
           click_button 'ログインする'
         end
 
-    context 'メールアドレスが未入力時に' do
-      it 'ユーザーの編集が失敗する'
-    end
-    context '登録済メールアドレスを使用' do
-      it 'ユーザーの編集が失敗'
-    end
-    context '他ユーザーの編集ページにアクセス' do
-      it 'アクセスが失敗する'
-    end
-  end
+        context 'メールアドレスが未入力時に' do
+          it 'ユーザーの編集が失敗する'
+        end
+        context '登録済メールアドレスを使用' do
+          it 'ユーザーの編集が失敗'
+        end
+        context '他ユーザーの編集ページにアクセス' do
+          it 'アクセスが失敗する'
+        end
+      end
 
-  describe 'マイページ' do
-    context 'タスクを作成' do
-      it '新規作成したタスクが表示される'
+      describe 'マイページ' do
+        context 'タスクを作成' do
+          it '新規作成したタスクが表示される'
+        end
+      end
     end
-  end
-end
