5bb60292adfef1677b209ee2edaef511a8a55091
Ryogo-Sasaki <burusenpai@gmail.com>
2020-03-17 15:51:00 +0900

    [Fix]system_spec(tasks)を修正する

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,88 +1,109 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system do
-  describe 'ログイン前'
   describe '挙動テスト' do
-    let(:user) { create(:user) }
-    let(:task) { create(:task, user_id: user.id) }
-    # let(other_user){(create(:user))}
-    # let(other_task){create(:task, user_id: other_user.id)}
+    let(:other_user) { create(:user) }
+    let(:other_task) { create(:task, user_id: other_user.id) }
 
     describe 'ログイン前のケース' do
       describe 'タスクの新規作成画面' do
-        context '権限がないため' do
-          it 'アクセスに失敗する'
-          user
-          visit new_task_path
-          expect(login_path).to eq(current_path)
-          expect(page).to have_content 'Login required'
+        context '権限なし' do
+          it 'アクセスに失敗する' do
+            visit new_task_path
+            expect(page).to have_content 'Login required'
+          end
+        end
+      end
+
+      describe 'タスクの編集画面' do
+        context '権限なし' do
+          it 'アクセスに失敗する' do
+            visit edit_task_path(user)
+            expect(page).to have_content 'Login required'
+          end
         end
       end
     end
+
+
+    describe 'ログイン後のケース' do
+      # before do
+      #   login(user)
+      # end
+      describe 'タスクの新規作成画面' do
+        context 'フォームの入力値がすべて入力されている時' do
+          it 'タスクの新規作成に成功する' do
+            visit login_path
+            fill_in 'Email', with: user.email
+            fill_in 'Password', with: 'password'
+            click_button("Login")
+            visit new_task_path
+            fill_in 'Title', with: 'タスク➀'
+            fill_in 'Content', with: 'コンテント➀'
+            select 'todo', from: 'Status'
+            click_button('Create Task')
+            expect(page).to have_content 'Task was successfully created'
+            expect(page).to have_content 'タスク➀'
+            expect(page).to have_content 'コンテント➀'
+          end
+        end
+      end
+    end
+
 
     describe 'タスクの編集画面' do
-      context '権限がないため' do
-        it 'アクセスに失敗する' do
-          user
-          visit edit_task_path(user)
-          expect(login_path).to eq(current_path)
-          expect(page).to have_content 'Login required'
+
+      context 'フォームの入力値が全て正しい場合' do
+        it 'タスクの編集に成功する' do
+          user = create(:user)
+          visit login_path
+          fill_in 'Email', with: user.email
+          fill_in 'Password', with: 'password'
+          click_button('Login')
+          visit edit_task_path(task)
+          fill_in 'Title', with: 'タスク➁'
+          fill_in 'Content', with: 'タスク➁'
+          select 'doing', from: 'Status'
+          fill_in 'Deadline', with: '2020/11/22 22:22'
+          click_button('Update Task')
+          expect(page).to have_content 'Task was successfully updated.'
+        end
+      end
+
+      context '他ユーザーの編集ページにアクセスした時' do
+        it '権限がないため、アクセス失敗' do
+          user = create(:user)
+          visit login_path
+          fill_in 'Email', with: user.email
+          fill_in 'Password', with: 'password'
+          click_button('Login')
+          visit edit_task_path(other_task)
+          expect(page).to have_content 'Forbidden access'
+        end
+      end
+    end
+
+
+    describe 'タスクの一覧画面' do
+      context 'Destroyボタンをクリックした時' do
+        it 'タスクの削除に成功する' do
+          user = create(:user)
+          visit login_path
+          fill_in 'Email', with: user.email
+          fill_in 'Password', with: 'password'
+          click_button('Login')
+          visit new_task_path
+          fill_in 'Title', with: 'タスク➀'
+          fill_in 'Content', with: 'コンテント➀'
+          select 'todo', from: 'Status'
+          click_button('Create Task')
+          visit tasks_path
+          click_link('Destroy')
+          click_button('OK')
+          expect(page).to have_content 'Task was successfully destroyed'
         end
       end
     end
   end
-
-  describe 'ログイン後のケース' do
-    describe 'タスクの新規作成画面' do
-      context 'フォームの入力値がすべて正しい時'
-      it 'タスクの新規作成に成功する'
-      login(user)
-
-      visit new_task_path
-      fill_in 'Title', with: '最初のタスク'
-      fill_in 'Content', with: '最初のコンテント'
-      select 'todo', from: 'Status'
-      click_button('Create Task')
-
-      expect(page).to have_content 'Task was successfully  created'
-    end
-  end
-
-
-describe '賛の編集画面' do
-
-  context 'フォームの入力ちが全て正しいとき' do
-    it 'タスクの編集に成功する' do
-      login(user)
-      visit edit_task_path(task)
-      fill_in 'Title', with: '2つめのタスクに変更'
-      fill_in 'Content', with: ''
-      select 'doing'
-      fill_in 'Deadline', with: 'eq December 201'
-      click_button('Update Task')
-      expect(page).to have_content 'Task was successfully updated'
-    end
-  end
-
-  content '他ユーザーの編集ページにアクセスした時'
-  it '権限がないため、アクセスが失敗する' do
-    login(user)
-    other_task
-    visit edit_task_path(other_task)
-    expect(root_path).to eq(current_path)
-    expect(page).to have_content 'Forbidden access'
-  end
 end
 
-describe 'タスクの一覧画面' do
-  let!(:task) { create(:task, user_id: user.id) }
-  context 'Destroyボタンをクリックした時'
-  it '賛の削除に成功する' do
-    login(user)
-    task
-    click_link('Destroy')
-    page.driver.browser.switch_to.alert.accept
-    expect(page).to have_content 'Task was successfully destroyed'
-  end
-end
-end
