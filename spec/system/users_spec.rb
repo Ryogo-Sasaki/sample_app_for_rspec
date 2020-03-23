36bcc760d19b3c70bbd8e0293e22400170551331
Ryogo-Sasaki <burusenpai@gmail.com>
2020-03-23 19:38:14 +0900

    [Delete]不要な改行を削除する

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -46,8 +46,7 @@ RSpec.describe "Users", type: :system do
           end
         end
       end
-
-
+      
       describe 'マイページ' do
         context 'ログインしていない状態' do
           it 'マイページへのアクセスが失敗する' do
